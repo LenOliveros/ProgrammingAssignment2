1f9c2362aef714ed27fe89be5964a9053b2fd9ae
Len Oliveros <45345057+LenOliveros@users.noreply.github.com>
2018-12-30 03:04:33 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions here are able to create and store a new matrix on the cche memory.
 
-## Write a short comment describing this function
+## To create the matrix we can use a function like this one
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ mtxinv <- NULL
+  setmtx <- function(m) {
+    x <<- m
+    mtxinv <<- NULL
+  }
+  getmtx <- function() x
+  setinvmtx <- function(inverse) mtxinv <<- inverse
+  getinvmtx <- function() mtxinv
+  list(setmtx = setmtx, getmtx = getmtx, setinvmtx = setinvmtx, getinvmtx = getinvmtx)
 }
 
 
-## Write a short comment describing this function
+## To solve the matrix previously created we can use the next function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mtxinv <- x$getinvmtx()
+  if(!is.null(mtxinv)) {
+    message("getting result")
+    return(mtxinv)
+  }
+  data <- x$getmtx()
+  mtxinv <- solve(data, ...)
+  x$setinvmtx(mtxinv)
+  mtxinv
 }
